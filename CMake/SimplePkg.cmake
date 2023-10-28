7e1357737fe39e99ca02d4fa50193c1cd0fff9f3
Ryan Pavlik <rpavlik@gmail.com>
2023-10-28 15:06:11 -0500

    simplepkg update

--- CMake/SimplePkg.cmake
+++ CMake/SimplePkg.cmake
@@ -1,18 +1,27 @@
 cmake_minimum_required(VERSION 3.25)
+
+if(simplepkg_POPULATED)
+  return()
+endif()
+
 include(FetchContent)
 
 function(simple_pkg NAME)
+  string(TOLOWER "${NAME}" lcname)
+
   # This lets us switch between file-package and package-name without switching
   # between simple_pkg and include
   string(FIND "${NAME}" "/" is_path)
   if(${is_path} GREATER -1)
     include(${NAME})
     return(PROPAGATE CMAKE_MODULE_PATH)
+  elseif(${${lcname}_POPULATED})
+    unset(${lcname}_INIT CACHE)
+    return()
   endif()
 
   cmake_parse_arguments(a NO_BUILD "" "" ${ARGN})
 
-  string(TOLOWER "${NAME}" lcname)
   message(STATUS "Simple package: ${NAME}")
 
   if(${NAME}_OVERRIDE)
@@ -24,7 +33,8 @@ function(simple_pkg NAME)
       message(FATAL_ERROR "Implement non-cmake building?")
     endif()
 
-    # Vaguely like FetchContent, but without _POPULATED
+    # Vaguely like FetchContent
+    set(${lcname}_POPULATED True)
     set(${lcname}_SOURCE_DIR ${${NAME}_OVERRIDE})
     set(${lcname}_BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/_override/${NAME}")
   else()
@@ -40,6 +50,8 @@ function(simple_pkg NAME)
   # Let packages add modules
   set(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}" PARENT_SCOPE)
 
+  # Exports
+  set(${lcname}_INIT True CACHE BOOL "" FORCE)
   set(${lcname}_POPULATED  "${${lcname}_POPULATED}" CACHE STRING "" FORCE)
   set(${lcname}_SOURCE_DIR "${${lcname}_SOURCE_DIR}" CACHE PATH "" FORCE)
   set(${lcname}_BINARY_DIR "${${lcname}_BINARY_DIR}" CACHE PATH "" FORCE)
@@ -56,14 +68,22 @@ function(simple_override NAME PATH)
   set(${NAME}_OVERRIDE "${PATH}" CACHE PATH "Override path for ${NAME}" FORCE)
 endfunction()
 
-if(NOT SIMPLEPKG_NO_DEFAULT_OVERRIDES)
-  include("$ENV{HOME}/.config/SimplePkg/overrides.cmake" OPTIONAL)
-  include("$ENV{HOME}/Documents/SimplePkg" OPTIONAL)
+if(NOT SIMPLEPKG_NO_GLOBAL_OVERRIDES)
+  include("$ENV{HOME}/.config/SimplePkg/pkg_overrides.cmake" OPTIONAL)
+  include("$ENV{HOME}/Documents/SimplePkg/pkg_overrides.cmake" OPTIONAL)
 else()
-  message(STATUS "SimplePkg NO default overrides")
+  message(STATUS "SimplePkg NO global overrides")
+endif()
+
+if(NOT SIMPLEPKG_NO_SOURCE_OVERRIDES)
+  include("${CMAKE_SOURCE_DIR}/.pkg_overrides.cmake" OPTIONAL)
+else()
+  message(STATUS "SimplePkg NO source overrides")
 endif()
 
 foreach(filename IN LISTS SIMPLEPKG_OVERRIDES)
   message(STATUS "SimplePkg load overrides from: ${filename}")
   include(${filename})
 endforeach()
+
+set(simplepkg_POPULATED True CACHE BOOL "" FORCE)
